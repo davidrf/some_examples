be6409a12db1d4e7bdd8ec8182edd4c78dc60344
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-09-08 23:50:42 -0400

    Initialize repository

---
+++ spec/features/launcher_index_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "", %(
+  As a User
+  I want to see a list of Launchers
+  So I can talk about their hairdos
+
+  #Acceptance Criteria
+  - I must see the name of all the Launchers in the database
+  - I should not see the description of their hairdos
+) do
+  scenario "" do
+  end
+end
+
